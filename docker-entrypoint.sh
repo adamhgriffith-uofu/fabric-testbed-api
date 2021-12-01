259a6322c1068cf338ab60a68163e05b896524c7
Adam Griffith <adam@cascadin.com>
2021-12-01 13:18:15 -0700

    New docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Enable strict mode:
+set -euo pipefail
+
+# Create the bash history file if necessary:
+if [ ! -f "$HISTFILE" ]
+then
+  touch /work/.bash_history_docker
+fi
+
+# Load environmental values:
+source "/docker/scripts/yml.sh"
+create_variables "/docker/envs/${FABRIC_ENV}.yml" "conf_"
+
+# Create the SSH configuration file:
+cat > "$HOME/.ssh/config" <<EOF
+### The External Fabric Bastion host
+Host fabric-bastion-host
+  HostName ${conf_fabric_bastion_hostname}
+  Port ${conf_fabric_bastion_port}
+  User ${FABRIC_API_USER}
+  IdentityFile /root/.ssh/id_rsa_fabric
+EOF
+
+# Create Slice key files:
+if [ ! -f "/root/.ssh/id_rsa_fabric_slice" ]
+then
+  ssh-keygen -b 2048 -t rsa -f "$HOME/.ssh/id_rsa_fabric_slice" -q -N ""
+fi
+
+# Start up Jupyter:
+cd "/work"
+jupyter notebook --no-browser --ip=0.0.0.0 --allow-root
