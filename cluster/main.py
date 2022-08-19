a8eca8aad4b5b3525c54debac7f0eb83c93e1c29
Adam H Griffith <adam.h.griffith@utah.edu>
2022-08-19 16:17:11 -0600

    slice submit still getting stuck with invalid refresh tokens.

--- cluster/main.py
+++ cluster/main.py
@@ -8,13 +8,13 @@ from logtools import CustomLogging
 cluster_logger = CustomLogging('cluster').get_logger()
 
 # Set up configuration:
-DEFAULT_FABRIC_CONFIG_LOCATION = f"{os.path.dirname(os.path.realpath(__file__))}/../fabric_config"
-cluster_logger.info(f'Applying config file: {DEFAULT_FABRIC_CONFIG_LOCATION}...')
+fabric_config_dir = f"{os.path.dirname(os.path.realpath(__file__))}/../fabric_config"
+cluster_logger.info(f'Applying config file: {fabric_config_dir}...')
 config = configparser.ConfigParser()
 try:
-    config.read(f"{DEFAULT_FABRIC_CONFIG_LOCATION}/fabric.conf")
+    config.read(f"{fabric_config_dir}/fabric.conf")
 except Exception as ex:
-    cluster_logger.error(f"Unable to read {DEFAULT_FABRIC_CONFIG_LOCATION}/fabric.conf!")
+    cluster_logger.error(f"Unable to read {fabric_config_dir}/fabric.conf!")
     raise ex
 
 # Set up OS environmental variables for FABlib:
@@ -24,24 +24,21 @@ os.environ['FABRIC_ORCHESTRATOR_HOST'] = 'orchestrator.fabric-testbed.net'
 os.environ['FABRIC_BASTION_HOST'] = 'bastion-1.fabric-testbed.net'
 os.environ['FABRIC_PROJECT_ID'] = config['secrets']['fabric_project_id']
 os.environ['FABRIC_BASTION_USERNAME'] = config['secrets']['fabric_bastion_username']
-os.environ[
-    'FABRIC_BASTION_KEY_LOCATION'] = f"{DEFAULT_FABRIC_CONFIG_LOCATION}/fabric-bastion-key"
-os.environ[
-    'FABRIC_SLICE_PRIVATE_KEY_FILE'] = f"{DEFAULT_FABRIC_CONFIG_LOCATION}/slice_key"
-os.environ[
-    'FABRIC_SLICE_PUBLIC_KEY_FILE'] = f"{DEFAULT_FABRIC_CONFIG_LOCATION}/slice_key.pub"
+os.environ['FABRIC_BASTION_KEY_LOCATION'] = f"{fabric_config_dir}/fabric-bastion-key"
+os.environ['FABRIC_SLICE_PRIVATE_KEY_FILE'] = f"{fabric_config_dir}/slice_key"
+os.environ['FABRIC_SLICE_PUBLIC_KEY_FILE'] = f"{fabric_config_dir}/slice_key.pub"
 os.environ['FABRIC_LOG_LEVEL'] = 'INFO'
-os.environ['FABRIC_LOG_FILE'] = f"{DEFAULT_FABRIC_CONFIG_LOCATION}/fablib.log"
-os.environ['FABRIC_TOKEN_LOCATION'] = f"{DEFAULT_FABRIC_CONFIG_LOCATION}/token.json"
+os.environ['FABRIC_LOG_FILE'] = f"{fabric_config_dir}/fablib.log"
+os.environ['FABRIC_TOKEN_LOCATION'] = f"{fabric_config_dir}/token.json"
 
 # Import the FABlib API:
 cluster_logger.info('Importing FABlib...')
 # TODO: Fix handling the pile of "Exception ignored in"s when executing fablib_manager().
 try:
     fablib = FablibManager()
+    fablib.show_config()
 except AttributeError:
     pass
-fablib.show_config()
 
 # Set up the Slice
 cluster_logger.info('Gathering Slice details...')
@@ -71,6 +68,29 @@ try:
     # Submit Slice request
     slice_id = fabric_slice.submit()
 except Exception as ex:
-    cluster_logger.error(f"{ex}")
+    cluster_logger.error(f"Exception: {ex}")
     raise ex
 
+# Display node attributes:
+# actual_fabric_slice = fablib.get_slice(slice_id=slice_id)
+# try:
+#     network = actual_fabric_slice.get_network(name=network_name)
+#     network_available_ips = network.get_available_ips()
+#     cluster_logger.info(f"{network}")
+# except Exception as ex:
+#     cluster_logger.error(f"Exception: {ex}")
+#     raise ex
+#
+# actual_nodes = actual_fabric_slice.get_nodes()
+# for index, name in enumerate(actual_nodes):
+#     node = actual_fabric_slice.get_node(name)
+#     iface = node.get_interface(network_name=network_name)
+#
+#     node1_address = network_available_ips.pop(0)
+#     node1_iface.ip_addr_add(addr=node1_address, subnet=network.get_subnet())
+#
+#     stdout, stderr = node1.execute(f'ip addr show {node1_iface.get_os_interface()}')
+#     print (stdout)
+#
+#     stdout, stderr = node1.execute(f'ip route list')
+#     print (stdout)
